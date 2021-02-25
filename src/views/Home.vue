f4e059801af59799948970950c51753065d170ed
wongyongren <wongyongren@hotmail.com>
2021-02-25 11:39:46 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,18 @@
 <template>
-  <div class="home shadow-xl bg-gray-200 w-8/12 mx-auto p-20">
+  
+  <!--<div class="home shadow-xl bg-gray-200 w-8/12 mx-auto p-20">
+    
     <h3 class="h-40 text-black" v-if="!(joke === 1) ">Stop</h3>
     <h3 class="h-40 text-black" v-else>Run</h3>
     <button
       class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-10 focus:outline-none"
       @click="addJoke"
     >On/Off</button>
-    <DisplayStatus />
-  </div>
-  
+
+
+  </div>-->
+
+      <DisplayStatus />
 </template>
 
 <script>
